7267c2f17c3323515bf46850b040d6ed5ba7a424
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-09-02 19:57:16 -0300

    INIT

---
+++ src/main/java/com/sentimentos/mc/SentimentosCoresApplication.java
@@ -0,0 +1,13 @@
+package com.sentimentos.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentosCoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentosCoresApplication.class, args);
+	}
+
+}
